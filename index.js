1efeb5e4744f465d270f9f489f19449f1a5e3abb
Igor Rakush <irakush@aol.com>
2023-11-02 14:23:43 -0400

    add lab 2

--- index.js
+++ index.js
@@ -28,3 +28,20 @@ function sayHiToHeadphonedRoommate(string){
   };
 
 };
+
+function introduction(name){
+  return "Hi, my name is ${name}.";
+};
+
+function logTwoValues(value1, value2) {
+  console.log(`The two values are ${value1} and ${value2}.`);
+};
+
+function introductionWithLanguage(name, language){
+  return "Hi, my name is ${name} and I am learning to program in ${language}.";
+};
+
+function sayHelloTo(firstName = "User") {
+  console.log(`Hello, ${firstName}!`);
+};
+
